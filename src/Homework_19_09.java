8d86a5a9d52a0c3029a920393d0bb8779133697d
commondrum <karoljhetman@gmail.com>
2023-09-19 20:15:43 +0200

    Next task done

--- src/Homework_19_09.java
+++ src/Homework_19_09.java
@@ -26,12 +26,6 @@ public class Homework_19_09 {
         return result;
     }
 
-    /*Utwórz statyczną funkcję splitToDigits przyjmującą
-     jako argument wartość całkowitą dodatnią i zwracającą jednowymiarową
-      tablicę wartości całkowitych dodatnich. Celem funkcji będzie utworzenie
-       tablicy o rozmiarze tożsamym z ilością cyfr liczby dostarczonej jako argument,
-     a następnie wypełnienie jej cyframi z jakich składa się dostarczona liczba.
-     */
 
     public static int[] splitToDigits(int number) {
 
